8162957a79b722eef545778159b8b839e527a50d
Jan Kiesewetter <jan@t3easy.de>
2014-08-15 10:29:07 +0200

    [TASK] Set upper maximum image width and height for content elements

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,5 @@
+<?php
+
+	// Override max image width and height
+$GLOBALS['TCA']['tt_content']['columns']['imagewidth']['config']['range']['upper'] = 1600;
+$GLOBALS['TCA']['tt_content']['columns']['imageheight']['config']['range']['upper'] = 1000;
